581a283d8bcbd60ffa3fa0df831616390ee10380
Mithilesh Chellappan <mithilesh153@gmail.com>
2024-12-31 12:54:06 +0530

    separate branch for frontend

--- backend/src/routes/audioRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateAudio } from '../controllers/audioController.js';
-
-const router = express.Router();
-
-router.post('/generate', generateAudio);
-
-export default router;
--- backend/src/routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { generateImage } from '../controllers/imageController.js';
-
-const router = express.Router();
-
-router.post('/generate', generateImage);
-
-export default router;
