a7cfe8b19f161c0c5408b844b29c54591fc39da2
Mithilesh Chellappan <mithilesh153@gmail.com>
2024-12-31 13:32:38 +0530

    env fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  return {
+    base: '/',
+    plugins: [react()],
+    define: {
+      'import.meta.env': import.meta.env,
+    }
+  }
+})
